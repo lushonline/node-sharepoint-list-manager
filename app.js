5988f954dc783c7cdc3132361c3822a1c5336f95
martinholden-skillsoft <12408585+martinholden-skillsoft@users.noreply.github.com>
2021-03-30 13:10:56 +0100

    Updates

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('dotenv-safe').config();
-
+const $REST = require('gd-sprest');
 const config = require('config');
 const fs = require('fs');
 const Path = require('path');
@@ -90,11 +90,6 @@ const main = async (configOptions) => {
       options.logger.error(`Error:  ${autherr}`, loggingOptions);
     });
 
-  const basequery = {
-    Select: ['Title', 'DESCRIPTION', 'MODALITY', 'LAUNCH'],
-    Top: 50,
-  };
-
   await sharepoint
     .getContextInfo(options)
     .then((response) => {
@@ -111,7 +106,34 @@ const main = async (configOptions) => {
       options.logger.error(`sharepoint.getContextInfo Error:  ${err}`, loggingOptions);
     });
 
+/*   await sharepoint
+    .createGenericList(options, 'Martin123456')
+    .then((response) => {
+      options.logger.info(
+        `sharepoint.createGenericList Response: ${stringifySafe(response)}`,
+        loggingOptions
+      );
+    })
+    .catch((err) => {
+      options.logger.error(`sharepoint.createGenericList Error:  ${err}`, loggingOptions);
+    }); */
+
   await sharepoint
+    .addFieldToList(options, 'Martin123456', {
+      Title: 'Description',
+      FieldTypeKind: $REST.SPTypes.FieldType.Text,
+    })
+    .then((response) => {
+      options.logger.info(
+        `sharepoint.addFieldToList Response: ${stringifySafe(response)}`,
+        loggingOptions
+      );
+    })
+    .catch((err) => {
+      options.logger.error(`sharepoint.addFieldToList Error:  ${err}`, loggingOptions);
+    });
+
+  /*   await sharepoint
     .addItem(options, { Title: 'Martin' })
     .then((response) => {
       options.logger.debug(
@@ -121,28 +143,61 @@ const main = async (configOptions) => {
     })
     .catch((err) => {
       options.logger.error(`sharepoint.addItem Error:  ${err}`, loggingOptions);
-    });
+    }); */
+
+  /*   await sharepoint
+    .addItems(options, [{ Title: 'Martin1', DESCRIPTION: 'Test' }, { Title: 'Martin2' }])
+    .then((response) => {
+      const responses = [...response];
+      _.forEach(responses, (item) => {
+        options.logger.debug(
+          `sharepoint.addItems Response: ${stringifySafe(item.data)}`,
+          loggingOptions
+        );
+      });
+    })
+    .catch((err) => {
+      options.logger.error(`sharepoint.addItems Error:  ${err}`, loggingOptions);
+    }); */
+
+  const basequery = {};
 
   await sharepoint
-    .getAllItems(
+    .upsertItems(
       options,
-      _.merge({}, basequery, {
-        Filter: odatafilter()
-          .fn('substringof', 'Title', 'Office', true, true)
-          .eq('MODALITY', 'WATCH')
-          .lt('MINUTES', 20)
-          .toString(),
-      })
+      [
+        { Title: 'Martin2', DESCRIPTION: 'Test2', LANGUAGE: 'Test2' },
+        { Title: 'Martin3', DESCRIPTION: 'Test3', LANGUAGE: 'Test3' },
+      ],
+      ['Title']
     )
     .then((response) => {
       options.logger.debug(
+        `sharepoint.upsertItems Response: ${stringifySafe(response.data)}`,
+        loggingOptions
+      );
+    })
+    .catch((err) => {
+      const message = accessSafe(() => JSON.stringify(err.response.data), err.message);
+      options.logger.error(`sharepoint.upsertItems Error:  ${message}`, loggingOptions);
+    });
+
+  /*   await sharepoint
+    .getAllItems(
+      options,
+      _.merge({}, basequery, {
+        Filter: odatafilter().fn('substringof', 'Title', 'Martin', true, true).toString(),
+      })
+    )
+    .then((response) => {
+      options.logger.info(
         `sharepoint.getAllItems Response: ${stringifySafe(response.data)}`,
         loggingOptions
       );
     })
     .catch((err) => {
       options.logger.error(`sharepoint.getAllItems Error:  ${err}`, loggingOptions);
-    });
+    }); */
 
   await sharepoint
     .getAllItems(options, _.merge({}, basequery))
@@ -151,6 +206,10 @@ const main = async (configOptions) => {
         `sharepoint.getAllItems Response: ${stringifySafe(response.data)}`,
         loggingOptions
       );
+      /*       const test = _.map(
+        accessSafe(() => response.data.d.results, []),
+        'UUID'
+      ); */
     })
     .catch((err) => {
       options.logger.error(`sharepoint.getItems Error:  ${err}`, loggingOptions);
